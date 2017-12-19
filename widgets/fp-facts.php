a7267c22286b3b268a879523fc82ad3d26a1a5f9
Roman <romanbondar@users.noreply.github.com>
2017-12-19 21:55:28 +0200

    Update fp-facts.php

--- widgets/fp-facts.php
+++ widgets/fp-facts.php
@@ -110,6 +110,7 @@ class Moesia_Facts extends WP_Widget {
 	
 	// display widget
 	function widget($args, $instance) {
+		echo $args['before_widget'];
 		$cache = array();
 		if ( ! $this->is_preview() ) {
 			$cache = wp_cache_get( 'moesia_facts', 'widget' );
@@ -193,6 +194,6 @@ class Moesia_Facts extends WP_Widget {
 		} else {
 			ob_end_flush();
 		}
+		echo $args['after_widget'];
 	}
-	
 }
