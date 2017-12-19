60edbf388e8fc360ac6d6eba4e9a368f4a478a0a
Roman <romanbondar@users.noreply.github.com>
2017-12-19 21:45:21 +0200

    Update fp-employees.php

--- widgets/fp-employees.php
+++ widgets/fp-employees.php
@@ -67,6 +67,7 @@ class Moesia_Employees extends WP_Widget {
 	
 	// display widget
 	function widget($args, $instance) {
+		echo $args['before_widget'];
 		$cache = array();
 		if ( ! $this->is_preview() ) {
 			$cache = wp_cache_get( 'moesia_employees', 'widget' );
@@ -207,6 +208,6 @@ class Moesia_Employees extends WP_Widget {
 		} else {
 			ob_end_flush();
 		}
+		echo $args['after_widget'];
 	}
-	
 }
